d306bf85ded231d380bb2bd508a9f57ece3fc44f
Nikita <maccuro.media@gmail.com>
2017-01-26 09:06:54 +0300

    refactored

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,12 @@
 import Vue from 'vue';
-let Quiz = require('./components/quiz.vue');
-
-var quiz = {
-  title: 'My quiz',
-  questions: [
-    {
-      text: "Question 1",
-      responses: [
-        {text: 'Wrong, too bad.'},
-        {text: 'Right!', correct: true},
-      ]
-    }, {
-      text: "Question 2",
-      responses: [
-        {text: 'Right answer', correct: true},
-        {text: 'Wrong answer'},
-      ]
-    }
-  ]
-};
+import Quiz from './components/quiz.vue';
 
 new Vue({
   el: '#app',
   components: {
-    'quiz': Quiz,
+    'quiz': Quiz
   },
   render: function (createElement) {
-    return createElement(Quiz)
-  },
-  // The view will trigger these methods on click
-  methods: {
-    // Go to next question
-    next: function() {
-      this.questionIndex++;
-    },
-    // Go to previous question
-    prev: function() {
-      this.questionIndex--;
-    },
-    // Return "true" count in userResponses
-    score: function() {
-      return this.userResponses.filter(function(val) { return val }).length;
-    }
+    return createElement(Quiz);
   }
 });
